e6d03e7e78bfe1229695185598d4f5ff5ab02344
adityapanwar027 <adityapanwar027@gmail.com>
2026-06-16 11:42:35 +0530

    Restore homepage message after CI/CD test

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRoutes);
 
 app.get("/", (req, res) => {
-  res.send("CICD test");
+  res.send("Job Application Tracker API is running...");
 });
 
 app.get("/health", (req, res) => {
